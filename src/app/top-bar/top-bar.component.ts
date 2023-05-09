cc78653e9d4f5de06c987c21d42cee32cc06d746
holywaterBetter <holywater.better@gmail.com>
2023-05-09 13:43:54 +0900

    feat(#1): top-bar padding 수정작업

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,20 +1,8 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss'],
 })
-export class TopBarComponent {
-  white: boolean;
-
-  constructor() {}
-
-  @HostListener('window:scroll', ['$event.target']) test() {
-    if (window.scrollY > 0.5) {
-      this.white = true;
-    } else {
-      this.white = false;
-    }
-  }
-}
+export class TopBarComponent {}
