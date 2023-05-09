c017684de2201c8f65da20cfa3a2b0717e8974d1
holywaterBetter <holywater.better@gmail.com>
2023-05-09 13:51:12 +0900

    feat(#1): home에서 video의 top을 top-bar 높이만큼 낮춤

--- src/app/home/home-video/home-video.component.ts
+++ src/app/home/home-video/home-video.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'home-video',
   templateUrl: './home-video.component.html',
+  styleUrls: ['./home-video.component.scss'],
 })
 export class HomeVideoComponent {}
