e4b02ebc081afe8cd8010d193133aaed5d75ae55
holywaterBetter <holywater.better@gmail.com>
2023-05-18 15:41:31 +0900

    feat(#3): story Component 계곡 구현

--- src/app/home/home-youtube/home-youtube.component.ts
+++ src/app/home/home-youtube/home-youtube.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'home-youtube',
   templateUrl: './home-youtube.component.html',
   styleUrls: ['./home-youtube.component.scss'],
 })
-export class HomeYoutubeComponent {}
+export class HomeYoutubeComponent {
+  @Input() code: string = 'rBiuJ_Usg10';
+}
