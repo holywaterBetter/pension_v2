2ddc64340e5e057b346c5f7057366a0ed43240e5
holywaterBetter <holywater.better@gmail.com>
2023-05-19 21:56:06 +0900

    feat(#4): food component 구현

--- src/app/pension-service/food/food.component.ts
+++ src/app/pension-service/food/food.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'food',
   templateUrl: './food.component.html',
-  styleUrls: ['./food.component.scss', '../app.component.scss'],
+  styleUrls: ['./food.component.scss', '../pension-service.scss'],
 })
 export class FoodComponent {}
